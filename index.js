c583812746f202c6cb688f67f93c10c88aff0397
kieuchicong99 <kieuchicong99@gmail.com>
2019-07-23 10:13:10 +0700

    update

--- index.js
+++ index.js
@@ -63,3 +63,33 @@ app.get('/webhook', (req, res) => {
     }
   }
 });
+
+setInterval(() => server.getConnections(
+  (err, connections) => console.log(`${connections} connections currently open`)
+), 1000);
+
+process.on('SIGTERM', shutDown);
+process.on('SIGINT', shutDown);
+
+let connections = [];
+
+server.on('connection', connection => {
+  connections.push(connection);
+  connection.on('close', () => connections = connections.filter(curr => curr !== connection));
+});
+
+function shutDown() {
+  console.log('Received kill signal, shutting down gracefully');
+  server.close(() => {
+      console.log('Closed out remaining connections');
+      process.exit(0);
+  });
+
+  setTimeout(() => {
+      console.error('Could not close connections in time, forcefully shutting down');
+      process.exit(1);
+  }, 10000);
+
+  connections.forEach(curr => curr.end());
+  setTimeout(() => connections.forEach(curr => curr.destroy()), 5000);
+}
