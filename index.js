ad13b7c3cb7f4f1015d0b8e6bc5dc6fa1c7b9b62
kieuchicong99 <kieuchicong99@gmail.com>
2019-07-24 09:51:55 +0700

    update

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.post('/webhook', (req, res) => {
       // will only ever contain one message, so we get index 0
       try {
         console.log("VNU-ENTRY:",entry);
-        let webhook_event = entry.messaging[0];
+        let webhook_event = entry.message[0];
         console.log(webhook_event);
       } catch (error) {
         console.log("VNU-ERROR:",error);        
